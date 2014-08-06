659fb1a348c516de75b1c65aae0d605a970ed8cd
Matt Neary <neary.matt@gmail.com>
2014-08-06 00:07:42 -0400

    additional parser functions for full CFG parsing

--- pan.js
+++ pan.js
@@ -6,11 +6,46 @@ var PResult = function(success, val, rest) {
 PResult.prototype.full = function() {
   return this.success && this.rest == "";
 };
-var parseChar = function(c) {
+var P = {};
+var parseChar = P.parseChar = function(c) {
   return function(text) {
     if( text[0] == c ) return new PResult(true, c, text.substr(1));
     return new PResult(false);
   };
 };
-exports.parseChar = parseChar;
+var unit = P.unit = function(x) {
+  return new PResult(true, "", x);
+};
+var bind = P.bind = function(a, b) {
+  if( a.success ) {
+    var bResult = b(a.rest);
+    return new PResult(bResult.success, a.val + bResult.val, bResult.rest);
+  } else {
+    return new PResult(false);
+  }
+};
+var Try = P.Try = function() {
+  var tries = [].slice.call(arguments);
+  return function(t) {
+    if( tries.length == 0 ) return new PResult(false);
+    var x = tries[0];
+    var p = x(t);
+    if( p.success ) return p;
+    else return Try.apply({},tries.slice(1))(t);
+  };
+};
+var product = P.product = function(a, b) {
+  return function(t) {
+    return bind(bind(unit(t), a), b);
+  };
+};
+var star = P.star = function(p) {
+  return Try(product(p, function(x) { return star(p)(x) }), unit);
+};
+var parseString = P.parseString = function(cs) {
+  return function(t) {
+    return cs.split("").map(parseChar).reduce(bind, unit(t));
+  };
+};
+module.exports = P;
 
--- tests.js
+++ tests.js
@@ -5,5 +5,28 @@ var fs = require("fs"),
 var aParser = P.parseChar("a");
 assert.equal(aParser("a").success, true);
 assert.equal(aParser("b").success, false);
+
+var aBoundParser = function(x) {
+  return P.bind(P.unit(x), P.parseChar("a"));
+};
+assert.equal(aBoundParser("a").success, true);
+assert.equal(aBoundParser("b").success, false);
+
+var abParser = function(x) {
+  return P.bind(P.bind(P.unit(x), P.parseChar("a")), P.parseChar("b"));
+};
+assert.equal(abParser("ab").success, true);
+assert.equal(abParser("ba").success, false);
+
+var aOrBParser = P.Try(P.parseChar("a"), P.parseChar("b"));
+assert.equal(aOrBParser("a").success, true);
+assert.equal(aOrBParser("b").success, true);
+assert.equal(aOrBParser("c").success, false);
+
+var parseAs = P.star(aParser);
+assert.equal(parseAs("a").success, true);
+assert.equal(parseAs("aa").success, true);
+assert.equal(parseAs("aab").full(), false);
+
 assert();
 
